406faa6ec871a44f0e3006199c3a65486307c143
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-11 21:37:18 +0800

    feat: function fetching pokemon

---
+++ src/utils/fetchUrl.js
@@ -0,0 +1,40 @@
+async function fetchApi(url) {
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(response.status)
+    }
+    return response.json()
+}
+
+async function fetchUrl (url) {
+    try {
+        const {results} = await fetchApi(url)
+        const linkFetch = []
+        results.forEach(p => {
+            linkFetch.push(fetchApi(p.url))
+        });
+        const data = await Promise.all(linkFetch)
+        const rests = []
+        for (let rest of data) {
+            let pokemon = {
+                name: "",
+                foto: "",
+                types : []
+            }
+            pokemon.name = rest.name
+            pokemon.foto = rest.sprites.front_default
+            const types = rest.types
+            for (let p in types) {
+                pokemon.types.push(rest.types[p].type.name)
+            }
+            rests.push(pokemon)
+        }
+        return rests
+        return
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+
+export default fetchUrl
