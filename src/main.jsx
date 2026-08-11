c918d0def019507ec44020e4d6d410d7a44b507d
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-11 14:22:06 +0800

    feat: create componen function use state

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Button from './pages/Button.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Button/>
+  </StrictMode>
 )
---
+++ src/pages/Button.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+function Button() {
+    const [num, setNum] = useState(0)
+    console.log(num)
+    
+    return (
+    <>
+    <form style={{
+        display: 'flex', 
+        flexDirection: 'column',
+        gap: '4px',
+        width: '50%',
+        height: '100vh',
+        margin: 'auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: 'solid 1px'
+    }} onSubmit={(e)=> {
+        e.preventDefault()
+        console.log(e.target.num.value)
+        setNum(parseInt(e.target.num.value) + num)
+    }}>
+        <label style={{ backgroundColor: '#f0f0f0', color: '#333' }} htmlFor="num">
+          Masukkan Angka:
+        </label>
+        <input type="number" name='num' id='num' />
+        <button>Add</button>
+        <span style={{
+            marginTop: '20px'
+        }}
+        >{num}</span>
+    </form>
+    </>
+  )
+}
+
+export default Button
