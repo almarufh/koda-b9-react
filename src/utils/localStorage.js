e9dbdf79eabd4b5e8d78de0659eca9a8607e31f1
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-12 14:12:46 +0800

    docs: jsdocs for sugest usege

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,14 +1,30 @@
+/**
+ * Save daat to Local Storage
+ * @param {string} key key want to save
+ * @param {Object[]} data data want to save in looragecal st
+ * @returns 
+ */
 function save (key, data) {
     window.localStorage.setItem(key, JSON.stringify(data))
     return 
 }
 
+/**
+ * Get data from local storage
+ * @param {string} key  key want to get
+ * @returns 
+ */
 function load (key) {
     let results = window.localStorage.getItem(key)
     results = JSON.parse(results)
     return results
 }
 
+/**
+ * Remove data from local storage
+ * @param {string} key key want to remove
+ * @returns 
+ */
 function remove (key) {
     window.localStorage.removeItem(key)
     return {
