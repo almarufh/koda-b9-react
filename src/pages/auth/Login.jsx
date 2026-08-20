dca3ffb95f448f5861152ee722d825a4f8b1951e
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-20 08:53:51 +0800

    fix: aved data actived

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -11,9 +11,16 @@ function Login() {
   const {register, handleSubmit, formState: { errors }} = useForm()
 
   const login = (e) => {
-      dispatch({type: "LOGIN", payload: e})
-      dispatch({type: "REGISTER", payload: e})
-      console.log("LOGIN", state)
+    const newUser = {
+      id: `${Date.now()}`,
+      email: "hidayatmaruf99@gmail.com",
+      name: e.email,
+      password: btoa(e.password),
+      role: "admin",
+      profile: "image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTEiIGhlaWdodD0iMTEiIHZpZXdCb3g9IjAgMCAxMSAxMSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTUuNSAxLjM3NUw2LjE4NzUgMy40Mzc1TDguMjUgNC4xMjVMNi4xODc1IDQuODEyNUw1LjUgNi44NzVMNC44MTI1IDQuODEyNUwyLjc1IDQuMTI1TDQuODEyNSAzLjQzNzVMNS41IDEuMzc1WiIgc3Ryb2tlPSIjRkY1RjIyIiBzdHJva2Utd2lkdGg9IjAuOTE2NjY3IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPHBhdGggZD0iTTguNzA4MzcgMS4zNzVMOS4wNTIxMiAyLjQwNjI1TDEwLjA4MzQgMi43NUw5LjA1MjEyIDMuMDkzNzVMOC43MDgzNyA0LjEyNUw4LjM2NDYyIDMuMDkzNzVMNy4zMzMzNyAyLjc1TDguMzY0NjIgMi40MDYyNUw4LjcwODM3IDEuMzc1WiIgc3Ryb2tlPSIjRkY1RjIyIiBzdHJva2Utd2lkdGg9IjAuOTE2NjY3IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg=="
+    }  
+    dispatch({type: "LOGIN", payload: newUser})
+    console.log(state.actived)
   }
 
     const passwordShow = () => {
