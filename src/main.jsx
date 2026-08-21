1282e60386081189f37577d0794823c34b48c325
almarufh_ <hidayatmaruf99@gmail.com>
2026-08-21 22:29:15 +0800

    feat: provider redux for can use all page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import './style.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import UsersProvider from './context/UsersProvider.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UsersProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UsersProvider>
+    <Provider store={store}>
+      <UsersProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UsersProvider>
+    </Provider>
   </StrictMode>
 )
